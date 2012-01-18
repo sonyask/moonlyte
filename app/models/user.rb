3b9298fc0f8342d0e74ff3dc8f05a3376657627b
Sonya Kim <sonyask@gmail.com>
2012-01-18 18:27:44 -0500

    Sonya's commit with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,9 +17,14 @@ class User < ActiveRecord::Base
   has_secure_password
   
   has_one :moonlyter, :dependent => :destroy
-  
+
   attr_accessible :firstName, :lastName, :email, :username, :avatar, :password, :password_confirmation
   
-  validates :username, :email, :password, :presence => true
+  validates :username, :email, :presence => true
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 7..40 }
   validates :username, :email, :uniqueness => true
+  validates :firstName, :lastName, :length => { :maximum => 20 }
+  
 end
